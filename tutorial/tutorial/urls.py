2fc192e666d0ada0039ad5a26314fd1a3f366205
Martin Ben <mebenav88@gmail.com>
2016-02-16 15:28:31 -0500

    refactoring to use viewsets, add routers

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -16,8 +16,22 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import url, include
+from snippets import views
+from rest_framework.routers import DefaultRouter
 
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+
+# The API URLs are now determined automatically by the router.
+# Additionally, we include the login URLs for the browsable API.
 urlpatterns = [
-    # url(r'^admin/', admin.site.urls),
-    url(r'^', include('snippets.urls')),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
+# urlpatterns = [
+#     # url(r'^admin/', admin.site.urls),
+#     url(r'^', include('snippets.urls')),
+# ]
